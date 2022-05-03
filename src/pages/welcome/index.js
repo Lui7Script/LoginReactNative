cff2fb6df6b92a3cf45a9fefcfb9666fc6f5be1b
Luiz Gustavo de Almeida Cruz - Kanguru Conexões Inteligentes <luiz.cruz@kan.guru>
2022-05-03 15:00:33 -0300

    first commit

---
+++ src/pages/welcome/index.js
@@ -0,0 +1,80 @@
+
+import React from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+    Image,
+    TouchableOpacity
+} from 'react-native';
+import { backgroundColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
+
+export default function Welcome() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.containerLogo}>
+                <Image
+                    source={require('../../assets/logo.png')}
+                    style={{ width: '100%' }}
+                    resizeMode="contain"
+                />
+            </View>
+            <View style={styles.containerForm}>
+                <Text style={styles.title} >Monitore, organize seus gastos de qualquer lugar</Text>
+                <Text>Faça o login para começar</Text>
+
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Acessar</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#38a69d'
+    },
+    containerLogo: {
+        flex: 2,
+        backgroundColor: '#38a69d',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    containerForm: {
+        flex: 1,
+        backgroundColor: '#fff',
+        borderTopLeftRadius: 25,
+        borderTopRightRadius: 25,
+        paddingStart: '5%',
+        paddingEnd: '5%'
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 28,
+        marginBottom: 12,
+    },
+    Text:{
+        color:'#a1a1a1'
+    },
+    button:{
+        position:'absolute',
+        backgroundColor:'#38a69d',
+        borderRadius:50,
+        paddingVertical:8,
+        width:'60%',
+        alignSelf:'center',
+        bottom:'15%',
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    buttonText:{
+        fontSize:18,
+        color:'#fff',
+        fontWeight:'bold'
+
+    }
+})
+
