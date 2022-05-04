021f5cbdbdfaff1994174cb01141ccf6ac225b75
Luiz Gustavo de Almeida Cruz - Kanguru Conexões Inteligentes <luiz.cruz@kan.guru>
2022-05-04 11:19:07 -0300

    lw

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,18 +1,40 @@
 
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 
 import * as Animatable from 'react-native-animatable'
+
 
 
 export default function SignIn() {
     return (
         <View style={styles.container}>
             <Animatable.View animation="fadeInLeft" delay={500} style={styles.containerHeader}>
-                <Text>Bem Vindo(a)</Text>
+                <Text style={styles.message}>Bem Vindo(a)</Text>
             </Animatable.View>
+
             <Animatable.View animation="fadeInUp" style={styles.containerForm}>
-                
+                <Text style={styles.title}>Email</Text>
+                <TextInput
+                    placeholder="Digite um email..."
+                    style={styles.input}
+                />
+
+                <Text style={styles.title}>Senha</Text>
+                <TextInput
+                    placeholder="Sua Senha"
+                    style={styles.input}
+                />
+
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Acessar</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.buttonRegister}>
+                    <Text style={styles.registerText}>Não possui uma conta?Cadastre-se</Text>
+                </TouchableOpacity>
+
+
 
             </Animatable.View>
         </View>
@@ -22,8 +44,64 @@ export default function SignIn() {
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
+        backgroundColor: '#38a69d'
+    },
 
+    containerHeader: {
+        marginTop: '14%',
+        marginBottom: '8%',
+        paddingStart: '5%'
+    },
 
+    message: {
+        fontSize: 28,
+        fontWeight: 'bold',
+        color: '#fff'
+    },
+
+    containerForm: {
+        backgroundColor: '#fff',
+        flex: 1,
+        borderBottomLeftRadius: 25,
+        borderBottomRightRadius: 25,
+        paddingStart: '5%',
+        paddingEnd: '5%'
+    },
+
+    title: {
+        fontSize: 20,
+        marginTop: 28
+    },
+
+    input: {
+        borderBottomWidth: 2,
+        height: 40,
+    },
+
+    button: {
+        backgroundColor: '#38a69d',
+        width: '100%',
+        borderRadius: 4,
+        paddingVertical: 8,
+        marginTop: 14,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    buttonText: {
+        color: '#fff',
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
+
+    buttonRegister: {
+        marginTop: 18,
+        alignSelf: 'center'
+    },
+
+    registerText:{
+        color:'#a1a1a1'
     }
 
 
