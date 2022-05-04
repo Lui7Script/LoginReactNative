5787e07b9ac39ac999464ca16b17a6923d556086
Luiz Gustavo de Almeida Cruz - Kanguru Conexões Inteligentes <luiz.cruz@kan.guru>
2022-05-04 09:17:45 -0300

    main

--- src/pages/welcome/index.js
+++ src/pages/welcome/index.js
@@ -7,26 +7,35 @@ import {
     Image,
     TouchableOpacity
 } from 'react-native';
-import { backgroundColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
+
+import * as Animatable from 'react-native-animatable'
+
+import { useNavigation } from '@react-navigation/native';
 
 export default function Welcome() {
+
+const navigation = useNavigation();
+
     return (
         <View style={styles.container}>
             <View style={styles.containerLogo}>
-                <Image
+                <Animatable.Image
+                    animation="flipInY"
                     source={require('../../assets/logo.png')}
                     style={{ width: '100%' }}
                     resizeMode="contain"
                 />
             </View>
-            <View style={styles.containerForm}>
-                <Text style={styles.title} >Monitore, organize seus gastos de qualquer lugar</Text>
+            <Animatable.View delay={600} animation="fadeInUp" style={styles.containerForm}>
+                <Text style={styles.title} >Monitore organize seus gastos de qualquer lugar</Text>
                 <Text>Faça o login para começar</Text>
 
-                <TouchableOpacity style={styles.button}>
+                <TouchableOpacity style={styles.button}
+                onPress= { () => navigation.navigate ('SignIn')}
+                >
                     <Text style={styles.buttonText}>Acessar</Text>
                 </TouchableOpacity>
-            </View>
+            </Animatable.View>
         </View>
     );
 }
@@ -56,24 +65,24 @@ const styles = StyleSheet.create({
         marginTop: 28,
         marginBottom: 12,
     },
-    Text:{
-        color:'#a1a1a1'
+    Text: {
+        color: '#a1a1a1'
     },
-    button:{
-        position:'absolute',
-        backgroundColor:'#38a69d',
-        borderRadius:50,
-        paddingVertical:8,
-        width:'60%',
-        alignSelf:'center',
-        bottom:'15%',
-        alignItems:'center',
-        justifyContent:'center'
+    button: {
+        position: 'absolute',
+        backgroundColor: '#38a69d',
+        borderRadius: 50,
+        paddingVertical: 8,
+        width: '60%',
+        alignSelf: 'center',
+        bottom: '15%',
+        alignItems: 'center',
+        justifyContent: 'center'
     },
-    buttonText:{
-        fontSize:18,
-        color:'#fff',
-        fontWeight:'bold'
+    buttonText: {
+        fontSize: 18,
+        color: '#fff',
+        fontWeight: 'bold'
 
     }
 })
